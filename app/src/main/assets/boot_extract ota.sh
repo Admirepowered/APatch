54233189b3ff52296492146a8aaa9c4308382c57
Admire <admire1971@outlook.com>
2024-03-09 18:30:59 +0800

    ll

--- app/src/main/assets/boot_extract ota.sh
+++ app/src/main/assets/boot_extract ota.sh
@@ -1,11 +1,16 @@
 #!/system/bin/sh
 
+#only flash to ota should check boot 
 ARCH=$(getprop ro.product.cpu.abi)
 
 # Load utility functions
 . ./util_functions.sh
 
 mount_partitions
+
+[ -z $SLOT ] && { >&2 echo "- can't determined current boot slot!"; exit 1; }
+
+exchange_boot_slot
 
 find_boot_image
 
