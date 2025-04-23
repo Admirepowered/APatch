16765a273a2bec1c11e6ee8fe56d6ff8a27f8cf4
Admirepowered <admire1971@outlook.com>
2025-04-24 00:34:18 +0800

    tigger in 0.11.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.11.1-dev")
+project.ext.set("kernelPatchVersion", "0.11.3")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 35
